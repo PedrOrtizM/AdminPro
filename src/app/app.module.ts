cf2add9b2678ad117474a23d4d857b3aa8a75db1
PedrOrtizM <pedrortizm18_@hotmail.com>
2019-02-13 14:37:55 -0500

    Modificaciones en los módulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,34 @@
+
+// Módulos
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { PagesModule } from './pages/pages.module';
 
+
+//Componentes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-
-
-//Rutas
-import { AppRoutingModule } from './app-routing.module';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register/register.component';
 
 
+
+
+//Rutas
+import { AppRoutingModule } from './app.routes';
+
+
+
 @NgModule({
-  declarations: [
+  declarations: [     
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    DashboardComponent,
-    ProgressComponent,
-    PagesComponent,
     RegisterComponent
+    
+
   ],
   imports: [
     BrowserModule,
+    PagesModule,
     AppRoutingModule
   ],
   providers: [],
